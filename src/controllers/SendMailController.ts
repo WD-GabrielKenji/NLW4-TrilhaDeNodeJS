a049575b30fefb796f40d732a40cbf6eb105b33c
WD-GabrielKenji <g.kenjiJS@hotmail.com>
2021-03-17 12:10:44 -0300

    NLW4-NodeJS: Refatoração no Controller do SendMail - alterando para reconhecer o id do E-mail a enviar, pelo id do survey

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -37,24 +37,24 @@ class SendMailController {
             });
         }
 
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs") // Variavel do Path (mapeador) / "(__dirname)" ira pegar o diretorio exato da aplicação / (".."(volta dois diretorios), define os diretorios até o arquivo ) Passamo o caminho até o arquivo 
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({ // Faz a verificação para duas condições, utilizando o where
+            where: { user_id: user.id, value: null }, // SELECT com Condição AND / Se existir o usuario, mas o valor da pesquisa for nulo
+            relations: ["user", "survey"],
+        });
+
         const variables = { // Variaveis necessarias para o envio do e-mail
             name: user.name,
             title: survey.title,
             description: survey.description,
-            user_id: user.id,
+            id: "",
             link: process.env.URL_MAIL // Variavel de URL do ".env"
         }
 
-        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs") // Variavel do Path (mapeador) / "(__dirname)" ira pegar o diretorio exato da aplicação / (".."(volta dois diretorios), define os diretorios até o arquivo ) Passamo o caminho até o arquivo 
-
-        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({ // Faz a verificação para duas condições, utilizando o where
-            where: [{user_id: user.id}, {value: null}], // Se existir o usuario, mas o valor da pesquisa for nulo
-            relations: ["user", "survey"],
-        });
-
         if(surveyUserAlreadyExists){
+            variables.id = surveyUserAlreadyExists.id; // Se existir o surveyUser
             await SendMailService.execute(email, survey.title, variables, npsPath);
-
             return response.json(surveyUserAlreadyExists);
         }
 
@@ -66,7 +66,7 @@ class SendMailController {
 
         await surveysUsersRepository.save(surveyUser); // Se tudo der certo
         // Enviar e-mail para o usuario
-        
+        variables.id = surveyUser.id; // Se não existir um surveyUser, dará o id
         await SendMailService.execute(email, survey.title, variables, npsPath) // Funçao para executar o service
 
         return response.json(surveyUser);
