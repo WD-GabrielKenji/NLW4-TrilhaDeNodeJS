daeb969b61c7b48bf7de6f1daa824fa4c4a33402
WD-GabrielKenji <g.kenjiJS@hotmail.com>
2021-03-17 16:01:28 -0300

    NLW4-NodeJS: Adicionado uma validação para o User utilizando a biblioteca Yup

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express'; // Fazendo os importes de Request e do Response para tipa-los dentro de create
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../repositories/UserRepository';
+import * as yup from 'yup';
 
 class UserController {
 
     async create(request: Request, response: Response){
         const {name, email} = request.body; // Corpo da requisição / Desestruturando o body e passando o name e email
+        
+        // Shape da validação
+        const schema = yup.object().shape({  // passamos o formato: OBJ e definimos o shape
+            name: yup.string().required(), // Sera um OBJ do tipo string e obrigatorio
+            email: yup.string().email().required() // Sera um OBJ do tipo Email e sera obrigatorio
+        })
+
+        // Validação do shape *Existem duas formas!*
+        /*
+        if(!(await schema.isValid(request.body))){ // Fazendo a validação do schema caso não for valido as informações do "request.body"
+            //return response.status(400).json({ error: "Validation Failed!" })
+        }
+        */
+        try{
+            await schema.validate(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err});
+        }
+
 
         const usersRepository = getCustomRepository(UserRepository); // Repassando o Repository de User
 
